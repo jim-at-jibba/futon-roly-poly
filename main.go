54c0592d6686119979a5d82b38d02fec958de512
James Best <jim@justjibba.net>
2022-12-15 14:40:58 +0000

    Add £ symbol

--- main.go
+++ main.go
@@ -42,8 +42,8 @@ func Scrape() {
 
 		fmt.Println(marks)
 		if marks != float64(current) {
-			SendMsg(fmt.Sprintf("Roly Poly has changed price %f", marks), os.Getenv("ELLA_PHONE_NUMBER"))
-			sendSlackMessage(fmt.Sprintf("Roly Poly has changed price %f", marks), "#36a64f")
+			SendMsg(fmt.Sprintf("Roly Poly has changed price £%f", marks), os.Getenv("ELLA_PHONE_NUMBER"))
+			sendSlackMessage(fmt.Sprintf("Roly Poly has changed price £%f", marks), "#36a64f")
 		}
 
 	})
